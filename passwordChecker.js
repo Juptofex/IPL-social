213161c660865e3d78716705211a4946f233e244
julien-damilot-vinci <julien.damilot@gmail.com>
2025-01-17 16:33:02 +0100

    feat: add special character validation to password validation function

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,5 +1,10 @@
 function PasswordValidator(password) {
+    const specialChar = /[!@#$%^&*()\-_=+{}[\]|:;<>.,?\/]/;
+
     if (password.length < 8) {
+        return false;
+    }
+    if (!specialChar.test(password)) {
         return false;
     }
     return true;
